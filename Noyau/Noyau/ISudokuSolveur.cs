037ebbe5e56babf1f44702842785baea87f2a5ce
Axel DE DEMANDOLX <axel.de-demandolx@edu.ece.fr>
2020-01-17 14:21:56 +0100

    Début du noyau / version 1

---
+++ Noyau/Noyau/ISudokuSolveur.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Noyau
+{
+    public interface ISudokuSolveur
+    {
+        Sudoku ResoudreSudoku(Sudoku s);
+    }
+}
