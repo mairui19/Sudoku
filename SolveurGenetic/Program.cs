a1769c357284c580c7a8b1ae69a827589fa7d81d
traor <mohamed.traore@edu.ece.fr>
2020-01-24 14:50:45 +0100

    Impementation solver  genetique

---
+++ SolveurGenetic/Program.cs
@@ -0,0 +1,21 @@
+﻿using Noyau;
+using System;
+using System.IO;
+
+namespace SolveurGenetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lignes = File.ReadAllLines(@"..\..\..\Sudoku_Easy50.txt");
+            var sudokus = Sudoku.ParseMulti(lignes);
+            var solveur = new SolverGeneticSharp();
+            var sudokuAResoudre = sudokus[0];
+            Console.WriteLine(sudokuAResoudre);
+            var solution1 = solveur.ResoudreSudoku(sudokuAResoudre);
+            Console.WriteLine(solution1);
+            Console.Read();
+        }
+    }
+}
