837959d3769b8e396e8b263907981123788646a5
sagan <sagana1@live.fr>
2020-01-24 15:04:39 +0100

    Création de la classe

--- SolveurORTools/Program.cs
+++ SolveurORTools/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Noyau;
+using System;
+using System.IO;
 
 namespace SolveurORTools
 {
@@ -6,7 +8,23 @@ namespace SolveurORTools
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lignes = File.ReadAllLines(@"..\..\..\Sudoku_Easy50.txt");
+            var sudokus = Sudoku.ParseMulti(lignes);
+            var solveur = new Solveur_tools();
+            Console.WriteLine(sudokus[0]);
+            var solution1 = solveur.ResoudreSudoku(sudokus[0]);
+            Console.WriteLine(solution1);
+            Console.Read();
         }
+
     }
+
+   
+
 }
+
+
+
+
+  
+
---
+++ SolveurORTools/Solveur_tools.cs
@@ -0,0 +1,16 @@
+﻿using Noyau;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.OrTools;
+
+namespace SolveurORTools
+{
+    public class Solveur_tools : ISudokuSolveur
+    {
+        public Sudoku ResoudreSudoku(Sudoku s)
+        {
+            return s;
+        }
+    }
+}
